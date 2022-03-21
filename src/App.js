0068f9188598b59ccdf83298aa20a7b82b7c703f
taufiqGit <wecannn29@gmail.com>
2022-03-22 03:05:55 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 function App() {
+  const [advice, setAdvice] = useState(null)
+  const getAdvice = async()=> {
+    try {
+      const res = await axios.get('https://api-advice.herokuapp.com/api')
+      // console.log(res.data.data)
+      setAdvice(res.data.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleRefreshAdvice = async()=>{
+    getAdvice()
+  }
+
+  useEffect(()=>{
+    getAdvice()
+  }, [])
+
+  //console.log(advice)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <div className="box-text">
+        <h3>advice {advice ? `#${advice.slip.id}` : '#---'}</h3>
+        <p>{advice ? `"${advice.slip.advice}"` : ''}</p>
+        <div className="hias">
+          <img className="" src="/images/pattern-divider-desktop.svg"/>
+        </div>
+        <button className="btn-refresh" onClick={handleRefreshAdvice}>
+          <img className="" src="/images/icon-dice.svg"/>
+        </button>
+      </div>
+    </main>
   );
 }
 
